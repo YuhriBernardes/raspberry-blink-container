80ba44f1e65161b955015c1ed45eeb64628097ff
Yuhri Bernardes <yuhri.graziano@gmail.com>
2021-03-25 17:37:32 -0300

    feat: add logs

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/YuhriBernardes/raspberry-blink-container
 
 go 1.16
 
-require github.com/stianeikeland/go-rpio v4.2.0+incompatible
+require (
+	github.com/sirupsen/logrus v1.8.1
+	github.com/stianeikeland/go-rpio v4.2.0+incompatible
+)
--- main.go
+++ main.go
@@ -1,22 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"time"
 
+	"github.com/sirupsen/logrus"
 	"github.com/stianeikeland/go-rpio"
 )
 
 var (
-	// Use mcu pin 10, corresponds to physical pin 19 on the pi
-	pin = rpio.Pin(10)
+// Use mcu pin 10, corresponds to physical pin 19 on the pi
 )
 
 func main() {
 	// Open and map memory to access gpio, check for errors
 	if err := rpio.Open(); err != nil {
-		fmt.Println(err)
+		logrus.WithError(err).Error("Failed to open RPIO device")
 		os.Exit(1)
 	}
 
@@ -24,11 +23,15 @@ func main() {
 	defer rpio.Close()
 
 	// Set pin to output mode
+	pinNumber := 10
+	pin := rpio.Pin(pinNumber)
+	logrus.Infof("Setting pin %d as output", pinNumber)
 	pin.Output()
 
 	// Toggle pin 20 times
 	for x := 0; x < 20; x++ {
 		pin.Toggle()
+		logrus.WithField("iteraction", x).Info("Pin toggled")
 		time.Sleep(500 * time.Millisecond)
 	}
 }
